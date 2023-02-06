926643854a99d9514715a1f9cf9401dfbae547fc
Julie Shapovalenko <jools.shapo@gmail.com>
2023-02-06 15:05:56 +0200

    add task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  colorName: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
+}
+
+refs.button.addEventListener('click', onBodyChangeColor)
+
+function onBodyChangeColor(event) {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.colorName.textContent = getRandomHexColor();
+}
+
